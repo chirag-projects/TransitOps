d06df0acacaed734cd7221113d51ba87c6583eda
prassr <92257326+prassr@users.noreply.github.com>
2026-07-12 16:21:16 +0530

    added role router

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routes import auth_router, user_router, vehicle_router
+from routes import role_router
 
 from model.model import create_tables
 
@@ -19,5 +20,6 @@ app.add_middleware(
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(user_router, prefix="/api/v1")
 app.include_router(vehicle_router, prefix="/api/v1")
+app.include_router(role_router, prefix="/api/v1")
 
 create_tables()
--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,3 +1,4 @@
 from .auth_routes import auth_router
 from .user_routes import user_router
 from .vehicle_routes import vehicle_router
+from .role_routes
